5709e893497052ed17c5ce085dac0d08b933af7d
Dhanush5005 <dhanushsankepally5@gmail.com>
2025-04-24 16:43:48 -0500

    this is first commit for this

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from './Hero';
+
+const AboutView = () => {
+    return (
+        <>
+            <Hero />
+            <h1>This is about us</h1>
+        </>
+    );
+}
+export default AboutView;
---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from './Hero';
+
+
+const Homepage = () => {
+    return (
+        <>
+            <Hero />
+            <h1>Hello world form react 201</h1>
+        </>
+    );
+}
+
+export default Homepage;
